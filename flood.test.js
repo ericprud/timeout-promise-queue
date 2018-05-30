316301482d1c8ab9d2e0e80b4554716b9125e974
Eric Prud'hommeaux <eric@w3.org>
2018-05-30 03:05:45 -0400

    ~ working in mocha and jest

--- flood.test.js
+++ flood.test.js
@@ -1,39 +1,68 @@
-let child_process = require('child_process');
-let AllTests = [];
+/*
+ */
+
+const QUEUE_SIZE = 'PROMISE_QUEUE_SIZE' in process.env
+      ? parseInt(process.env.PROMISE_QUEUE_SIZE)
+      : 25
+const Threads = 100
+const Sleep = 1000
+const ThreadTimeout = 20000
+const SuiteTimeout = 4 * Sleep * Threads / QUEUE_SIZE
+const Command = 'setTimeout(() => { process.exit(0); }, ' + Sleep + ')'
+// const Command = 'for (let i = 0; i < 2**28; ++i) ;' // The busy alternative.
+
+const child_process = require('child_process')
+const AllTests = []
+const Queue = require('./promise-queue.js').PromiseQueue(QUEUE_SIZE)
+const _AfterAllTests = typeof jest !== 'undefined' ? afterAll : after
 
 /* start processes */
-for (let i = 0; i < 100; ++i) {
+for (let i = 0; i < Threads; ++i) {
   AllTests.push({
     i: i,
     start: new Date(),
-    exec: new Promise((resolve, reject) => {
-      let program = child_process.spawn(
-        'node', ['-e', 'setTimeout(() => { process.exit(0); }, 9999)'])
-        // 'node', ['-e', 'for (let i = 0; i < 2**28; ++i) ;'])
+    exec: Queue.add(cancel => new Promise((resolve, reject) => {
+      let program = child_process.spawn('node', ['-e', Command])
       program.on('exit', exitCode => { resolve({exitCode:exitCode}) })
-      program.on('error', err => { reject(err) })
-    })
+      program.on('error', reject)
+      cancel.on('timeout', err => {
+        program.kill()
+        reject()
+      })
+    }), ThreadTimeout, () => Error('timeout exceeded in test ' + i))
   })
 }
 
 /* test results */
 describe('churn', () => {
   AllTests.forEach(test => {
-    it('should execute test ' + test.i + '.',
-       done => {
-         test.exec.then(exec => {
-           test.end = new Date()
-           done()
-         })
-       })
+    let title = 'should execute test ' + test.i + '.'
+    function cb (done) {
+      test.exec.then(exec => {
+        test.end = new Date()
+        test.message = 'OK'
+        done()
+      }).catch(e => {
+        test.end = new Date()
+        test.message = 'Error: ' + e
+        done(e)
+      })
+        }
+    if (typeof jest !== 'undefined') {
+      it(title, cb, SuiteTimeout)
+    } else /*if (typeof mocha !== 'undefined')*/ {
+      it(title, cb).timeout(SuiteTimeout)
+    }
+    // Need to dispose of these rejections before cb is called.
+    test.exec.catch(e => null)
   })
 })
 
-if (false)
-afterAll(() => {
-  console.log()
-  AllTests.forEach(test => {
-    console.log(test.i, (test.end - test.start)/1000.0)
-  })
+if (true)
+_AfterAllTests(() => {
+  console.log('\n' + AllTests.map(
+    test =>
+      test.i + ' ' + (test.end - test.start)/1000.0 + ' ' + test.message
+  ).join('\n'))
 })
 
---
+++ promise-queue.js
@@ -0,0 +1,72 @@
+
+const EventEmitter = require('events');
+
+function PromiseQueue (threshold) {
+  let queue = []
+  let inPlay = 0
+
+  return {
+    /** queue (I/O) functions which return promises.
+     * @pfunc returns a promise
+     */
+    add: function (pfunc, timeout, rejection) {
+      if (++inPlay > threshold) {
+        return new Promise((resolve, reject) => {
+          queue.push(() => {
+            resolve(makeTimeout())
+          })
+        })
+      } else {
+        return makeTimeout()
+      }
+
+      function makeTimeout () {
+        let timer = null
+        let clientCancellation = new EventEmitter();
+        let myCancellation = new EventEmitter();
+        let ret = timeout === undefined
+          ? pfunc()
+          : Promise.race([
+            new Promise((resolve, reject) => {
+              timer = setTimeout(() => {
+                let r = typeof rejection === 'undefined'
+                    ? Error('timeout of ' + timeout + ' exceeded')
+                    : typeof rejection === 'function'
+                    ? rejection()
+                    : rejection
+                clientCancellation.emit('timeout', r);
+                nextEntry()
+                reject(r)
+              }, timeout)
+              myCancellation.on('clear', resolve)
+              return timer
+            }),
+            pfunc(clientCancellation).then(result => {
+              myCancellation.emit('clear');
+              clearTimeout(timer)
+              return result
+            })
+          ])
+        return ret.then(nextEntry)
+      }
+    },
+
+    /** number of entries in the queue.
+     */
+    size: function () {
+      return queue.length
+    }
+  }
+
+  function nextEntry (ret) {
+    // After each resolution, check the queue.
+    --inPlay
+    if (queue.length > 0) {
+      queue.pop()()
+    }
+    return ret
+  }
+}
+
+module.exports = { PromiseQueue: PromiseQueue }
+
